83af80c2427055b67695f05b06eaa6251341317e
Ho 229 <2189684957@qq.com>
2021-07-10 16:29:58 +0800

    Update-CI

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -11,15 +11,6 @@ param (
 # vcToolsRedistDir: ${{ steps.build.outputs.vcToolsRedistDir }}
 # msvcArch: ${{ matrix.msvc_arch }}
 
-
-# winSdkDir: C:\Program Files (x86)\Windows Kits\10\ 
-# winSdkVer: 10.0.19041.0\ 
-# vcToolsInstallDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.28.29333\ 
-# vcToolsRedistDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.28.29325\ 
-# VCINSTALLDIR: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC
-# archiveName: 5.9.9-win32_msvc2015
-# msvcArch: x86
-
 $scriptDir = $PSScriptRoot
 $currentDir = Get-Location
 Write-Host "currentDir" $currentDir
